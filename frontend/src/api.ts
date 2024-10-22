03a1c2379cc83bda419ec322f3b23435bae8ca0d
azrulwm <azrulazmi.wm@gmail.com>
2024-10-22 10:07:01 +0800

    fix: Fixed bug not attaching the correct token to api request

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constant";
-import Cookie from "js-cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,7 +7,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = Cookie.get(ACCESS_TOKEN);
+    const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
